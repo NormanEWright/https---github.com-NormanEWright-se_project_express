7be27131905320d2c583a06ffe3ac0806a101163
= <n.wright.online@gmail.com>
2022-11-26 15:43:19 -0500

    Setup app to use routes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/wtwr_db', (r) => {
   console.log('Connected to DB')}, e => console.log('DB error', e));
 
+const routes = require('./routes');
+app.use(express.json());
+app.use(routes);
+
 app.listen(PORT, () => {
   console.log(`App listening at port: ${PORT}`);
 });
