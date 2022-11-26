1264ce2741591ad961c720ce1e3b211dc76e3408
= <n.wright.online@gmail.com>
2022-11-26 12:50:42 -0500

    Add options to db connect()

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const { PORT = 3001 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/wtwr_db');
+mongoose.connect('mongodb://localhost:27017/wtwr_db', (r) => {
+  console.log('Connected to DB')}, e => console.log('DB error', e));
 
 app.listen(PORT, () => {
   console.log(`App listening at port: ${PORT}`);
