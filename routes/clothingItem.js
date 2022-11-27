5a1cd89d180a9a4e3e7222fe7ed0e01709e0c530
= <n.wright.online@gmail.com>
2022-11-27 11:20:08 -0500

    Add clothingItem routes

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
 const { createItem, getItems, updateItem, deleteItem } = require('../controllers/clothingItem');
 
-// CREATE
-router.post('/', createItem);
-
-// READ
 router.get('/', getItems);
-
-// UPDATE
+router.post('/', createItem);
 router.put('/:itemId', updateItem);
-
-// DELETE
 router.delete('/:itemId', deleteItem);
 
 module.exports = router;
