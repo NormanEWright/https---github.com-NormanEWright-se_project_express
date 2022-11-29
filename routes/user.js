83d82e21a02f82605a6a138aceac93d7b3ea4ea4
= <n.wright.online@gmail.com>
2022-11-28 21:27:46 -0500

    Edit user router

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { getUsers, getUserById, createUser } = require('../controllers/users');
 
-router.get('/user', getUsers);
-router.get('/user/:userId', getUserById);
+router.get('/', getUsers);
+router.get('/:userId', getUserById);
 router.post('/', createUser);
 
 module.exports = router;
