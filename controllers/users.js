d492c271868b759a680959116e892b79a18bf8fa
= <n.wright.online@gmail.com>
2022-11-28 21:25:23 -0500

    Replace userId with _id

--- controllers/users.js
+++ controllers/users.js
@@ -2,20 +2,25 @@ const User = require('../models/user');
 
 // Get all users
 const getUsers = (req, res) => {
+  console.log(req.body);
+
   User.find({})
     .then((users) => {
       console.log(users)
       res.status(200).send({ User: users })
     })
-    .catch((e) => res.status(500).send({ message: 'Error from getUsers' }));
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).send({ message: 'Error from getUsers' })
+    });
 }
 
 // Get user by id
 const getUserById = (req, res) => {
-  const { userId } = req.body._id;
-  console.log(userId);
+  const { _id } = req.user._id;
+  console.log(_id);
 
-  User.findById({ userId })
+  User.findById({ _id })
     .orFail()
     .then((user) => res.status(200).send({ User: user }))
     .catch((e) => res.status(500).send({ message: 'Error from getUserById' }));
@@ -24,11 +29,15 @@ const getUserById = (req, res) => {
 // Create a user
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
+  console.log(name, avatar);
   User.create({ name, avatar })
     .then((user) => {
       res.status(200).send({ User: user });
     })
-    .catch((e) => res.status(500).send({ message: 'Error from createUser', e }));
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).send({ message: 'Error from createUser', e })
+    });
 }
 
 module.exports = {
